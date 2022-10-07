a4c221f46ba41d860518953f2a3af76a119e846a
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-10-07 14:43:51 +0700

    1

---
+++ Range/Range.js
@@ -0,0 +1,8 @@
+
+var load = (data) =>{
+  ran1 = document.getElementById("range1").value
+  ran2 = document.getElementById("range2").value  
+  ran3 = document.getElementById("range3").value  
+  bg = document.getElementById("zone_color")
+  bg.style. background =  `rgb(${ran1},${ran2},${ran3}`
+}
